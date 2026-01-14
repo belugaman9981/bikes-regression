34704d4aecf221e9cdc1bd3511290a80c5a56044
belugaman9981 <littledage@outlook.com>
2026-01-13 21:43:55 -0800

    Update fcc-biikes-regression.py

--- Codes/fcc-biikes-regression.py
+++ Codes/fcc-biikes-regression.py
@@ -7,3 +7,26 @@ import matplotlib.pyplot     as plt
 from   sklearn.linear_model  import LinearRegression
 from   sklearn.preprocessing import StandardScaler
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" Dataset:
+    
+    Dua, D. and Graff, C. (2019). UCI Machine Learning Repository 
+    [http://archive.ics.uci.edu/ml]. Irvine, CA: University of California, 
+    School of Information and Computer Science.
+    
+    Source: Data Source: http://data.seoul.go.kr/ SOUTH KOREA PUBLIC HOLIDAYS. URL: publicholidays.go.kr """
+    
