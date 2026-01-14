90225314eee1ab1c8194b733b1e3ea5bf6863281
belugaman9981 <littledage@outlook.com>
2026-01-13 21:39:13 -0800

    Update fcc-biikes-regression.py

--- Codes/fcc-biikes-regression.py
+++ Codes/fcc-biikes-regression.py
@@ -1,4 +1,9 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import copy
+import numpy                 as np
+import pandas                as pd
+import seaborn               as sns
+import tensorflow            as tf
+import matplotlib.pyplot     as plt
+from   sklearn.preprocessing import StandardScaler
+from   sklearn.linear_model  import LinearRegression
 
