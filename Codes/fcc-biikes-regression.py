fc089e74811dc0036b6568e44b008d7b8cedf1bf
belugaman <littledage@outlook.com>
2026-01-15 20:55:34 -0800

    Commit

--- Codes/fcc-biikes-regression.py
+++ Codes/fcc-biikes-regression.py
@@ -65,7 +65,9 @@ _, X_train_test, y_train_test = get_xy(test,  "bike_count", x_labels= ["temp"])
 temp_reg = LinearRegression()
 temp_reg.fit(X_train_temp, y_train_temp)
 
-print(temp_reg.coef_, temp_reg.intercept_)
+temp_reg.score(X_test, y_test)
+
+# time: 2:50:00
 
 
 
