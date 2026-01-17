85404cf4bdc31abe90f6fac9e31c1ae3450bf8b9
belugaman <littledage@outlook.com>
2026-01-16 20:07:21 -0800

    Update fcc-biikes-regression.py

--- Codes/fcc-biikes-regression.py
+++ Codes/fcc-biikes-regression.py
@@ -78,6 +78,104 @@ plt.ylabel("Bike Count")
 plt.xlabel("Temperature (C)")
 plt.show()
 
+<<<<<<< FourthPartofLesson
+
+df.head()
+
+
+# train-test valid
+
+train, val, test = np.split(df.sample(frac=1), [int(.6*len(df)), int(.8*len(df))])
+
+
+def get_xy(dataframe, y_label, x_labels= None):
+    dataframe = copy.deepcopy(dataframe)
+
+    if x_labels is None:
+        X = dataframe[[c for c in dataframe.columns if c != y_label]].values
+
+    else:
+        if len(x_labels) == 1:
+            X = dataframe[[x_labels]].values
+
+        else:
+            X = dataframe[x_labels].values
+
+    y = dataframe[y_label].values.reshape(-1, 1)
+    data = np.hstack((X, y))
+
+    return data, X, y
+        
+
+_, X_train_temp, y_train_temp = get_xy(train, "bike_count", x_labels= ["temp"])
+_, X_train_val,  y_train_val  = get_xy(val,   "bike_count", x_labels= ["temp"])
+_, X_train_test, y_train_test = get_xy(test,  "bike_count", x_labels= ["temp"])
+
+
+temp_reg = LinearRegression()
+temp_reg.fit(X_train_temp, y_train_temp)
+
+temp_reg.score(X_train_test, y_train_test)
+
+# time: 2:50:00
+
+plt.scatter(X_train_temp, y_train_temp, label= "Data", color='blue')
+x = tf.linspace(-20, 40, 100)
+plt.plot(x, temp_reg.predict(np.array(x).reshape(-1, 1)), label= "Fit", color= 'red', linewidth= 3)
+plt.legend()
+plt.title("Bikes vs Temp")
+plt.ylabel("Bike Count")
+plt.xlabel("Temperature (C)")
+plt.show()
+
+
+# Multiple Linear Regression
+
+
+train, val, test = np.split(df.sample(frac=1), [int(.6*len(df)), int(.8*len(df))])
+
+
+_, X_train_all, y_train_all  = get_xy(train, "bike_count", x_labels= df.columns[1:])
+_, X_train_all, y_train_all  = get_xy(val,   "bike_count", x_labels= df.columns[1:])
+_, X_train_all, y_train_all  = get_xy(test,  "bike_count", x_labels= df.columns[1:])
+
+all_reg = LinearRegression()
+all_reg.fit(X_train_all, y_train_all)
+
+all_reg.score(X_train_all, y_train_all)
+
+
+# Reggression With Neural Networks
+
+def plot_history(history):
+    plt.plot(history.history['loss'], label='loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
+    plt.set_xlabel('Epoch')
+    plt.set_ylabel('MSE')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+temp_norm = tf.keras.layers.Normalization(input_shape= [1,], axis= None)
+temp_norm.adapt(X_train_temp.reshape(-1))
+
+temp_model = tf.keras.Sequential([
+    temp_norm,
+    tf.keras.layers.Dense(1)
+    
+])
+
+temp_model.compile(optimizer= tf.keras.optimizers.Adam(learning_rate= 0.10),
+                       loss= 'mean_squared_error')
+
+history = temp_model.fit(X_train_temp, y_train_temp, 
+                         epochs= 1000, verbose=0, 
+                         validation_data= (X_train_temp, y_train_temp))
+=======
+>>>>>>> main
+
+
 
 """ Dataset:    
     
